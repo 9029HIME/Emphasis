b0e9dcaec06e0680e353775a3c124729871e7125
koujyungenn <513137767@qq.com>
2022-07-04 13:55:08 +0800

    Initial commit

---
+++ spring_emphasis/src/main/java/com/genn/spring/spring_emphasis/SpringEmphasisApplication.java
@@ -0,0 +1,13 @@
+package com.genn.spring.spring_emphasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmphasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmphasisApplication.class, args);
+    }
+
+}
