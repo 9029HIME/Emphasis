c0b27d7681e479d6651a1e352c95e484054c1446
koujyungenn <513137767@qq.com>
2022-07-18 22:19:59 +0800

    MySQL知识点开篇草稿

---
+++ mysql_emphasis/src/main/java/com/genn/mysql_emphasis/MysqlEmphasisApplication.java
@@ -0,0 +1,13 @@
+package com.genn.mysql_emphasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlEmphasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlEmphasisApplication.class, args);
+    }
+
+}
---
+++ mysql_emphasis/src/test/java/com/genn/mysql_emphasis/MysqlEmphasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genn.mysql_emphasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlEmphasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
