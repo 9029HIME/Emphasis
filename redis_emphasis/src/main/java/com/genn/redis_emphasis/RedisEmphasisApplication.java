3586eaa16f06dcba21f9aa7a23e1307aa4e8057f
koujyungenn <513137767@qq.com>
2022-07-24 16:24:49 +0800

    Redis知识点的开篇

---
+++ redis_emphasis/src/main/java/com/genn/redis_emphasis/RedisEmphasisApplication.java
@@ -0,0 +1,13 @@
+package com.genn.redis_emphasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisEmphasisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisEmphasisApplication.class, args);
+    }
+
+}
---
+++ redis_emphasis/src/test/java/com/genn/redis_emphasis/RedisEmphasisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genn.redis_emphasis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisEmphasisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
